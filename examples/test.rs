1c77b8b74514591125ca3e12c4ac60d81b873a6c
funnsam <funnsam8@gmail.com>
2023-02-01 14:47:44 +0000

    kinda works??? wot i done werong

--- examples/test.rs
+++ examples/test.rs
@@ -2,13 +2,11 @@ use bare_learning::{network::*, randomizer::init_randomizer};
 
 fn main() {
     init_randomizer();
-    let mut nn = Network::new(2, &vec![2, 1]).unwrap();
-    for _ in 0..100000 {
+    let mut nn = Network::new(1, &vec![2, 1]).unwrap();
+    for i in 0..10000 {
         nn.train(&vec![Data { inputs: vec![0.1_f32, 0.3_f32], outputs: vec![0.2_f32] }, Data { inputs: vec![0.6_f32, 0.9_f32], outputs: vec![0.75_f32] }], 1.0);
+        println!("\n---------------------{i}\n{:?}", nn);
         print!("{:?}", nn.predict(&vec![0.1_f32, 0.3_f32]).unwrap());
-        print!("{:?}", nn.predict(&vec![0.6_f32, 0.9_f32]).unwrap());
-        println!("");
+        println!("{:?}", nn.predict(&vec![0.6_f32, 0.9_f32]).unwrap());
     }
-
-    println!("{:?}", nn);
 }
--- src/network.rs
+++ src/network.rs
@@ -36,25 +36,32 @@ impl Layer {
         results
     }
 
+
+    // (a, z)
+    fn pred_w_z(&self, prev: &Vec<f32>) -> Vec<(f32, f32)> {
+        let mut results = Vec::new();
+        for i in 0..self.len {
+            let bias = self.bias[i];
+            let mut sum = 0.0_f32;
+            for j in 0..self.p_len {
+                sum += prev[j] * self.weights[j][i];
+            }
+            results.push((sigmoid(bias + sum), bias + sum))
+        }
+        results
+    }
+
     fn train(&mut self, data: &Vec<Data>, alpha: f32) {
         fn loss(a: f32, b: f32) -> f32 {
             (a - b).powi(2)
         }
 
-        fn error(expect: &Vec<f32>, result: &Vec<f32>) -> f32 {
-            let mut a = 0.0;
-            for i in 0..expect.len()-1 {
-                a += loss(expect[i], result[i])
-            }
-            a / expect.len() as f32
-        }
-
         let mut _results = Vec::with_capacity(data.len());
         for i in data.iter() {
-            _results.push(self.predict(&i.inputs));
+            _results.push(self.pred_w_z(&i.inputs));
         }
-        let _results = _results.iter().cloned().flat_map(|a| a.iter().cloned().collect::<Vec<_>>()).collect::<Vec<f32>>();
-        let mut results: Vec<Vec<f32>> = vec![Vec::with_capacity(_results.len() >> 1); 2];
+        let _results = _results.iter().cloned().flat_map(|a| a.iter().cloned().collect::<Vec<_>>()).collect::<Vec<(f32, f32)>>();
+        let mut results: Vec<Vec<(f32, f32)>> = vec![Vec::with_capacity(_results.len() >> 1); 2];
 
         for (i, el) in _results.iter().enumerate() {
             results[i&1].push(*el)
@@ -69,12 +76,20 @@ impl Layer {
         }
 
         for (i, bias) in self.bias.iter_mut().enumerate() {
-            *bias -= alpha * (error(&expect[i], &results[i]) / *bias);
+            let mut t = 0.0;
+            for r in results.iter() {
+                t += (r[i].1 / *bias) * r[i].0 * r[i].1
+            }
+            *bias -= t / results.len() as f32 * alpha;
         }
 
         for node in self.weights.iter_mut() {
-            for (i, weight) in node.iter_mut().enumerate() {
-                *weight -= alpha * (error(&expect[i], &results[i]) / *weight);
+            for (j, weight) in node.iter_mut().enumerate() {
+                let mut t = 0.0;
+                for (i, r) in results.iter().enumerate() {
+                    t += loss(r[j].0, expect[j][i]) * r[j].0 * r[j].1
+                }
+                *weight -= t / results.len() as f32 * alpha
             }
         }
     }
@@ -83,7 +98,6 @@ impl Layer {
 /// This is a neural network.
 #[derive(Debug)]
 pub struct Network {
-    input : Vec<f32>,
     hidden: Vec<Layer>,
     output: Layer
 }
@@ -91,19 +105,17 @@ pub struct Network {
 impl Network {
     /// Used for creating a neural network. Layers
     /// defines how many layers that a neural network
-    /// contains (counts input and output). Sizes
-    /// defines how large each layer is (also counts in
-    /// and output layers).
+    /// contains (counts input). Sizes
+    /// defines how large each layer is (also counts input layers).
     pub fn new(layers: usize, sizes: &Vec<usize>) -> Option<Self> {
-        if layers != sizes.len() || layers < 2 || sizes.len() < 2 {
+        if layers != sizes.len()-1 || layers < 1 || sizes.len() < 2 {
             return None
         }
         let mut netw = Self {
-            input : vec![0.0; sizes[0]],// batch_f32(sizes[0], 1.0, 0.5),
             hidden: Vec::with_capacity(sizes.len()),
             output: Layer::new(sizes[sizes.len()-1], sizes[sizes.len()-2])
         };
-        for i in 0..layers-2 {
+        for i in 0..layers-1 {
             netw.hidden.push(Layer::new(sizes[i], sizes[i+1]))
         }
         Some(netw)
@@ -111,15 +123,11 @@ impl Network {
 
     /// Predict an answer.
     pub fn predict(&self, data: &Vec<f32>) -> Option<Vec<f32>> {
-        if data.len() != self.input.len() {
-            return None
-        }
+        // if data.len() != self.input.len() {
+        //     return None
+        // }
 
-        let mut prev_iter = Vec::with_capacity(self.input.len());
-        for (i, el) in self.input.iter().enumerate() {
-            prev_iter.push(el + data[i])
-        }
-
+        let mut prev_iter = data.clone();
         for el in self.hidden.iter() {
             prev_iter = el.predict(&prev_iter);
         }
@@ -129,7 +137,7 @@ impl Network {
 
     /// Trains a neural network. Alpha is the learning rate
     pub fn train(&mut self, data: &Vec<Data>, alpha: f32) -> Option<()> {
-        if data[0].inputs.len() != self.input.len() || data[0].outputs.len() != self.output.len {
+        if data[0].outputs.len() != self.output.len {
             return None
         }
 
