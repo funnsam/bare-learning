c926316e77c26f9c5e14f1f8868247a96a317a56
funnsam <funnsam8@gmail.com>
2023-02-04 06:57:41 +0000

    stuff

--- examples/test.rs
+++ examples/test.rs
@@ -1,25 +1,42 @@
 use bare_learning::{network::*, randomizer::init_randomizer, utils::*};
+use std::thread::*;
 
 fn main() {
     init_randomizer();
-    let mut nn = Network::new(1, &vec![2, 1]).unwrap();
-    print_stuff(&nn, 0);
-    for i in 1..=100 {
-        let time = train(&mut nn).as_micros();
-        print_stuff(&nn, i * 10000);
-        println!("Training 10,000 times took {time}micro secs, avg: {}ms.", time / 10000)
+
+    let mut handles = Vec::new();
+    for i in 1..=10 {
+        handles.push(
+            spawn(move || {
+                per_thread(i);
+            })
+        )
     }
+
+    for _ in 0..handles.len() {
+        handles.pop().unwrap().join().unwrap()
+    }
+}
+
+fn per_thread(thread_id: usize) -> Network {
+    let mut nn = Network::new(1, &vec![2, 1]).unwrap();
+    print_stuff(&nn, 0, thread_id);
+    for i in 1..=100 {
+        let _ = train(&mut nn).as_micros();
+        print_stuff(&nn, i * 10000, thread_id);
+    }
+    nn
 }
 
 fn train(nn: &mut Network) -> std::time::Duration {
     let start = std::time::SystemTime::now();
     for _ in 0..10000 {
-        nn.train(&vec![Data { inputs: vec![0.1, 0.3], outputs: vec![0.2] }, Data { inputs: vec![0.6, 0.9], outputs: vec![0.75] }, Data { inputs: vec![0.2, 0.4], outputs: vec![0.3] }], 0.15);
+        nn.train(&vec![Data { inputs: vec![0.1, 0.3], outputs: vec![0.2] }, Data { inputs: vec![0.6, 0.9], outputs: vec![0.75] }, Data { inputs: vec![0.2, 0.4], outputs: vec![0.3] }], 0.01);
     }
     start.elapsed().unwrap()
 }
 
-fn print_stuff(nn: &Network, i: usize) {
+fn print_stuff(nn: &Network, i: usize, n: usize) {
     let pred = nn.predict(&vec![0.4, 0.6]).unwrap()[0];
-    println!("\x1b[0;1m{i:010} ---- {:4.5}%\x1b[0m ---- {pred:03.25}", (1.0-loss(pred, 0.5)) * 100.0);
+    println!("\x1b[0;1;32m{n:02}: \x1b[0;1m{i:010} ---- {:4.5}%\x1b[0m ---- {pred:03.5}", (1.0-loss(pred, 0.5)) * 100.0);
 }
